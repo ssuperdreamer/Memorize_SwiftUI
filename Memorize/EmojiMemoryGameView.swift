0164d54ed4852482f003fcedf68cabe06d869dc0
yanghongxiang <ssuperdreamer@gmail.com>
2021-09-19 20:07:35 -0400

    Lecture 5: GeometryReader: Adapt View

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -14,7 +14,7 @@ struct EmojiMemoryGameView: View {
     var body: some View {
         VStack{
             ScrollView {
-                LazyVGrid(columns:[GridItem(.adaptive(minimum: 60))]) {
+                LazyVGrid(columns:[GridItem(.adaptive(minimum: 100))]) {
                     ForEach(game.cards) { card in
                         CardView(card: card).aspectRatio(2/3, contentMode: .fit).onTapGesture {
                             game.choose(card)
@@ -31,22 +31,33 @@ struct CardView: View {
     let card: EmojiMemoryGame.Card
     
     var body: some View {
-        ZStack {
-            let shape = RoundedRectangle(cornerRadius: 20)
-            if card.isFaceUp {
-                shape.fill().foregroundColor(.white)
-                shape.strokeBorder(lineWidth: 3)
-                Text(card.content).font(Font.system(size: 100))
-            } else if(card.isMatched) {
-                shape.opacity(0)
-            } else {
-                shape.fill()
+        GeometryReader(content: { geometry in
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+                if card.isFaceUp {
+                    shape.fill().foregroundColor(.white)
+                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                    Text(card.content)
+                        .font(font(in: geometry.size))
+                } else if(card.isMatched) {
+                    shape.opacity(0)
+                } else {
+                    shape.fill()
+                }
             }
-        }
-        
-//        GeometryReader(content: { geometry
-//
-//        })
+        })
+    }
+    
+    
+    
+    private func font(in size: CGSize) -> Font {
+        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 20.0
+        static let lineWidth: CGFloat = 3
+        static let fontScale: CGFloat = 0.8
     }
 }
 
